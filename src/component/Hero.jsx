ae0cbbe2837c43c93f7713ca3dc5929f698b7a28
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2026-01-04 17:05:51 +0100

    update

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div
       className="  bg-cover bg-center h-70 mt-53 md:h-105 md:mt-46.5 border-2 border-amber-600"
-      style={{ backgroundImage: `url('public/slider (2).svg')` }}
+      style={{ backgroundImage: `url('public/HeroImage.svg')` }}
     ></div>
   );
 };
