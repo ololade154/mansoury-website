c8c790808729087076a14f8d92618c0f909ae74f
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2026-01-04 20:59:58 +0100

    update

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,9 +1,15 @@
 const Hero = () => {
   return (
     <div
-      className="  bg-cover bg-center h-70 mt-53 md:h-105 md:mt-46.5 border-2 border-amber-600"
-      style={{ backgroundImage: `url('/HeroImage.svg')` }}
-    ></div>
+      className=" bg-cover bg-center h-70 mt-53 md:h-105 md:mt-46.5 "
+      style={{
+        backgroundImage: `url('/HeroImage.svg')`,
+      }}
+    >
+      <h1 className="white-text text-center lato text-[24px] px-4 md:text-[48px] py-2  font-bold">
+        Ehance Your Home with convience and Style
+      </h1>
+    </div>
   );
 };
 export default Hero;
