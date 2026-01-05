ef727b7dc0b1e432055f81cdafaac4b2756fbb30
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2026-01-06 00:54:44 +0100

    update

---
+++ src/component/Carousel.jsx
@@ -0,0 +1,84 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+
+const Carousel = () => {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 400,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 0,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="slider-container max-w-full overflow-hidden  ">
+      <Slider {...settings}>
+        <div className="relative">
+          <img
+            src="/HeroImage.svg"
+            alt="slider"
+            className="object-cover object-center h-72 md:h-105 w-full block"
+          />
+          <div className="absolute inset-0 flex justify-center">
+            <h1 className="white-text text-center lato text-[24px] px-4 md:text-[48px] py-4 font-bold">
+              Ehance Your Home with convience and Style
+            </h1>
+          </div>
+        </div>
+        <div className="relative">
+          <img
+            src="/HeroImage.svg"
+            alt="slider"
+            className="object-cover object-center h-72 md:h-105 w-full block"
+          />
+          <div className="absolute inset-0 flex justify-center">
+            <h1 className="white-text text-center lato text-[24px] px-4 md:text-[48px] py-4 font-bold">
+              Ehance Your Home with convience and Style
+            </h1>
+          </div>
+        </div>
+        <div className="relative">
+          <img
+            src="/HeroImage.svg"
+            alt="slider"
+            className="object-cover object-center h-72 md:h-105 w-full block "
+          />
+          <div className="absolute inset-0 flex justify-center">
+            <h1 className="white-text text-center lato text-[24px] px-4 md:text-[48px] py-4 font-bold">
+              Ehance Your Home with convience and Style
+            </h1>
+          </div>
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,14 +1,20 @@
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+import Carousel from '../component/Carousel';
 const Hero = () => {
   return (
-    <div
-      className=" bg-cover bg-center h-60 mt-30 md:h-105 md:mt-46.5"
-      style={{
-        backgroundImage: `url('/HeroImage.svg')`,
-      }}
-    >
-      <h1 className="white-text text-center lato text-[24px] px-4 md:text-[48px] py-4 font-bold">
-        Ehance Your Home with convience and Style
-      </h1>
+    // <div
+    //   className=" bg-cover bg-center h-60 mt-30 md:h-105 md:mt-46.5"
+    //   style={{
+    //     backgroundImage: `url('/HeroImage.svg')`,
+    //   }}
+    // >
+    //   <h1 className="white-text text-center lato text-[24px] px-4 md:text-[48px] py-4 font-bold">
+    //     Ehance Your Home with convience and Style
+    //   </h1>
+    // </div>
+    <div className="  mt-30 md:mt-46.5">
+      <Carousel />
     </div>
   );
 };
