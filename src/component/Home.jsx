a895a96899c437a5bc2d3fa89594d149825fd1ab
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2026-01-11 17:08:00 +0100

    update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,12 +1,14 @@
 import Header from '../component/Header';
 import Hero from '../component/Hero';
 import ProductOne from './ProductOne';
+import Display from '../component/Display';
 const Home = () => {
   return (
     <>
       <Header />
       <Hero />
       <ProductOne />
+      <Display />
     </>
   );
 };
--- src/component/ProductOne.jsx
+++ src/component/ProductOne.jsx
@@ -2,7 +2,7 @@ import ProductOneSlider from '../component/ProductOneSlider';
 
 const ProductOne = () => {
   return (
-    <div className="body-color flex-col flex md:flex-row overflow-hidden p-4 md:p-8">
+    <div className="body-color flex-col flex md:flex-row overflow-hidden py-4 px-8 md:py-6 md:px-16">
       <div className="flex-none md:w-1/3">
         <h2>Discover Hot Deals For Your Home!</h2>
       </div>
