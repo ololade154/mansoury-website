97ab95dfc9e53ebec218b0affc40c97c9367bb86
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2026-01-11 17:26:57 +0100

    update

--- src/component/Display.jsx
+++ src/component/Display.jsx
@@ -1,27 +1,24 @@
 const Display = () => {
   return (
     <div className="grid md:grid-cols-3 gap-4 md:gap-3 md:grid-rows-2 w-full py-4 px-8 md:py-6 md:px-16">
-      <div className="h-56 md:h-auto">
-        <img src="/microwave.svg" className=" w-full  h-full object-cover " />
+      <div className="h-64 md:h-auto">
+        <img src="/microwave.svg" className=" w-full h-full object-cover " />
       </div>
-      <div className="h-56 md:h-auto">
-        <img src="/Pot.svg" className=" w-full  h-full object-cover " />
+      <div className="h-64 md:h-auto">
+        <img src="/Pot.svg" className=" w-full h-full object-cover " />
       </div>
-      <div className="h-56 md:h-auto">
-        <img
-          src="/travelling-bag.svg"
-          className="w-full  h-full object-cover"
-        />
+      <div className="h-64 md:h-auto">
+        <img src="/travelling-bag.svg" className="w-full h-full object-cover" />
       </div>
       <div className="md:col-span-3 ">
         <div className="grid md:grid-cols-2 gap-4 md:gap-3">
-          <div className="h-56 md:h-auto">
+          <div className="h-64 md:h-auto">
             <img
               src="/Refrigerator.svg"
-              className="w-full  h-full object-cover "
+              className="w-full h-full object-cover "
             />
           </div>
-          <div className="h-56 md:h-auto">
+          <div className="h-64 md:h-auto">
             <img
               src="/washing-machine.svg"
               className="w-full h-full object-cover"
